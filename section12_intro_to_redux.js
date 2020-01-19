970639753650678c5d849d01d53d240f75998ad7
Robbie Colborne <robbie_c1000@hotmail.com>
2020-01-19 17:42:28 +1100

    first commit

---
+++ section12_intro_to_redux.js
@@ -0,0 +1,80 @@
+/*
+WHAT IS REDUX? 
+
+1. Redux is a state management library. When we say state, we mean the same state we have used so far.
+We are going to make redux in charge of handling data inside our applications. 
+2. Redux makes creating complex applications a little bit easier. 
+3. We don't have to use redux on every project that we work on!
+4. The redux library itself is not designed just to work with REACT. 
+*/
+
+/*
+REDUX BY ANALOGY
+
+Redux Cycle
+
+Action Creator -> Action -> Dispatch -> Reducers -> State. 
+
+Analogy 
+
+We are going to build an insurance company. 
+
+- Customers hold a "policy", if bad stuff happens to them then they are payed money.
+- a "claim", is something a customer files to an insurance company. They do this to get some money from an insurance company. 
+
+
+
+
+*/
+
+
+/*
+A BIT MORE ANALOGY 
+
+
+*/
+
+/*
+FINISHING THE ANALOGY 
+
+
+*/
+
+/*
+MAPPING THE ANALOGY TO REDUX
+
+
+*/
+
+
+/*
+MODELING WITH REDUX
+
+
+*/
+
+
+/*
+CREATING REDCUCERS
+
+
+*/
+
+
+/*
+RULES OF REDUCERS
+
+
+*/
+
+
+/*
+TESTUNG OUR EXAMPLE
+
+*/
+
+/*
+IMPORTANT REDUX NOTES 
+
+*/
+
